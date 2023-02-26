3f0ad45ab46dc73904f2c9f50a0e17e1f5d9328d
Rick Mota <ricardorickricardosmt@gmail.com>
2023-02-26 09:13:23 -0300

    adicionado toda a parte do responsivo

--- src/js/menu.js
+++
@@ -1,8 +0,0 @@
-let btnCheckbox = document.getElementsByTagName("input")[0]
-console.log(btnCheckbox)
-
-let menu = document.getElementsByClassName("menu-lateral")[0]
-
-btnCheckbox.addEventListener('click', function() {
-    menu.classList.add("menu-lateral")
-})
